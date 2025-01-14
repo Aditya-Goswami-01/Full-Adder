c944f81465cb003bed6a25efe049fcec2df845bd
Aditya-Goswami-01 <goswamiadi890@gmail.com>
2025-01-14 10:59:49 +0530

    adder.v

--- adder.v
+++ adder.v
@@ -1,7 +1,7 @@
-`timescale 1s / 1s
-module adder(input A,B,C, output reg sum, carry);
-    always @(A or B or C) begin
-        sum = A^B^C;
-        carry = (A&B)|(B&C)|(A&C);
-        end
-endmodule
+`timescale 1s / 1s        // define the time unit and time precision
+module adder(input A,B,C, output reg sum, carry);       // create a module with name adder and also define the input and output pins.
+    always @(A or B or C) begin                // Behaviroal structure is begin.
+        sum = A^B^C;                              // here, I assign the sum output in terms of input 
+        carry = (A&B)|(B&C)|(A&C);                // here, I assign the carry output in terms of input
+        end                                       // Ending of behaviroal structure. 
+endmodule                                        // every module is end with end module.
